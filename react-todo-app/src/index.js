0b9172e602198e11cc0a19f5ddcb7f5b953caad5
Hubert MUGABO <mugberto@gmail.com>
2021-08-17 01:19:17 +0200

    Initial commit

--- react-todo-app/src/index.js
+++ react-todo-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello from React!!!</h1>;
+const element = <h1>Hello from React!</h1>
 
 ReactDOM.render(element, document.getElementById('root'));
