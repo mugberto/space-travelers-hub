3f31a46b92540812e54da46214457a4311e27608
Lameck1 <lameck72@gmail.com>
2021-08-31 12:49:56 +0300

    Refactor Header Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import { NavLink } from 'react-router';
+import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
 
-const Header = () => {
-  return (
-    <header>
-      <div className="brand-logo"></div>
-      <nav>
-        <NavLink to='/rockets'>Rockets</NavLink>
-      </nav>
-    </header>
-  );
+const Header = ({ logo, routes }) => (
+  <header>
+    <div className="brand-logo">
+      <img src={logo} alt="logo" height="100" width="100" />
+    </div>
+    <nav>
+      <ul>
+        {routes.map(({ name, path }) => (
+          <li key={path} className="nav-link">
+            <NavLink exact to={path}>{name}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  </header>
+);
+
+Header.propTypes = {
+  logo: PropTypes.string.isRequired,
+  routes: PropTypes.arrayOf(PropTypes.shape({
+    path: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  })).isRequired,
 };
+
+export default Header;
