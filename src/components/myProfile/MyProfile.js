e9aecbf22b60e5837a93dbf28aee7a52c2b28aa4
Lameck1 <lameck72@gmail.com>
2021-09-01 13:22:33 +0300

    Display joined missions in Profile

--- src/components/myProfile/MyProfile.js
+++ src/components/myProfile/MyProfile.js
@@ -1,4 +1,20 @@
-const MyProfile = () => (
-  null
-);
+import { useSelector } from 'react-redux';
+import './my-profile.css';
+
+const MyProfile = () => {
+  const myMissions = useSelector((state) => state.missions.filter((mission) => mission.reserved));
+  return (
+    <div className="profile-data">
+      <div className="data">
+        <h3>My Missions</h3>
+        <ul className="list">
+          {myMissions.map(({ id, name }) => (
+            <li key={id} className="list-item">{name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
 export default MyProfile;
