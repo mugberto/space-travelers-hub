178f3a7b53b886f60f9378872672e11a3e32da83
Hubert MUGABO <mugberto@gmail.com>
2021-09-01 22:55:59 +0200

    Display rocket list

--- src/components/rocket/Rockets.js
+++ src/components/rocket/Rockets.js
@@ -1,13 +1,65 @@
-import { useSelector } from 'react-redux';
-import { List, ListItem, ListItemText } from '@material-ui/core';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  List, ListItem, Card, CardMedia, CardContent, Typography, Button,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+import { fetchRocketData } from '../../redux/rocket/rocket';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+  },
+  details: {
+    flex: '1 1 auto',
+  },
+  media: {
+    flex: '0 0 212px',
+    height: '164px',
+    objectFit: 'cover',
+  },
+}));
 
 const Rockets = () => {
   const rockets = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!rockets.length) {
+      dispatch(fetchRocketData());
+    }
+  }, []);
+
+  const classes = useStyles();
+
   return (
     <List>
       {rockets.map((rocket) => (
         <ListItem key={rocket.id}>
-          <ListItemText primary={rocket.description} />
+          <Card className={classes.root} style={{ boxShadow: 'none' }}>
+            <CardMedia
+              className={classes.media}
+              image={rocket.flickr_images[0]}
+              title={rocket.rocket_name}
+            />
+            <div className={classes.details}>
+              <CardContent style={{ padding: '0 1rem' }}>
+                <Typography component="h6" variant="h6">
+                  {rocket.rocket_name}
+                </Typography>
+                <Typography
+                  component="p"
+                  color="textSecondary"
+                >
+                  {rocket.description}
+                </Typography>
+              </CardContent>
+              <div className={classes.rBtn}>
+                <Button variant="contained" color="primary" style={{ margin: '1rem' }}>
+                  Reserve a ticket
+                </Button>
+              </div>
+            </div>
+          </Card>
         </ListItem>
       ))}
     </List>
