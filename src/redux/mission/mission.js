61d65f89558fa3129638a9d6567093ead3f0764c
Lameck1 <lameck72@gmail.com>
2021-09-01 08:40:07 +0300

    Implement mission joining & mission leaving

--- src/redux/mission/mission.js
+++ src/redux/mission/mission.js
@@ -1,10 +1,22 @@
 import fetchMissions from './fetchMissions';
 
 const LOAD_MISSIONS = 'missions/load';
+const JOIN_MISSION = 'missions/join';
+const LEAVE_MISSION = 'missions/leave';
 
 const loadMissions = (payload) => ({
   type: LOAD_MISSIONS,
   payload,
+});
+
+export const joinMission = (id) => ({
+  type: JOIN_MISSION,
+  id,
+});
+
+export const leaveMission = (id) => ({
+  type: LEAVE_MISSION,
+  id,
 });
 
 export const getMissions = async (dispatch) => {
@@ -22,6 +34,20 @@ const reducer = (state = [], action) => {
   switch (action.type) {
     case LOAD_MISSIONS:
       return action.payload;
+    case JOIN_MISSION:
+      return state.map((mission) => {
+        if (mission.id !== action.id) {
+          return mission;
+        }
+        return { ...mission, reserved: true };
+      });
+    case LEAVE_MISSION:
+      return state.map((mission) => {
+        if (mission.id !== action.id) {
+          return mission;
+        }
+        return { ...mission, reserved: false };
+      });
     default:
       return state;
   }
