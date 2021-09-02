67cd11bed48dd1bac8e04454a9acef5d66d4c46c
Hubert MUGABO <mugberto@gmail.com>
2021-09-02 21:13:24 +0200

    Add interactive tests for Rocket component

--- src/components/rocket/__tests__/Rockets.test.js
+++ src/components/rocket/__tests__/Rockets.test.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import { render } from '../../../test-utils';
+import { render, screen, fireEvent } from '../../../test-utils';
 import Rockets from '../Rockets';
 
 describe('Rockets', () => {
   it('renders page correctly', () => {
     const tree = render(<Rockets />);
     expect(tree).toMatchSnapshot();
+  });
+
+  it('updates the UI with loaded Rockets', async () => {
+    render(<Rockets />);
+    expect(await screen.findByText('Falcon 1')).toBeInTheDocument();
+  });
+
+  it('displays "Reserve Rocket" button', async () => {
+    render(<Rockets />);
+    expect(await screen.findByText('Reserve Rocket')).toBeInTheDocument();
+  });
+
+  it('turns Reserve Rocket button to Cancel Reservation button after click', async () => {
+    render(<Rockets />);
+    expect(await screen.findByText('Reserve Rocket')).toBeInTheDocument();
+    fireEvent.click(screen.getByRole('button', { name: 'Reserve Rocket' }));
+    expect(await screen.findByText('Cancel Reservation')).toBeInTheDocument();
   });
 });
